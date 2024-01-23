584db61b72f554b5de118a51a400a6ef6f5a7b52
ArthurSegato <github.reentry594@passinbox.com>
2024-01-23 22:05:12 +0000

    Fix security headers

--- src/routes/plugin@csp.tsx
+++ src/routes/plugin@csp.tsx
@@ -11,7 +11,7 @@ export const onRequest: RequestHandler = (event) => {
     `img-src 'self' 'unsafe-inline' data:`,
     `script-src 'self' 'unsafe-inline' https: 'nonce-${nonce}' 'strict-dynamic'`,
     `style-src 'self' 'unsafe-inline'`,
-    `frame-src 'self' 'nonce-${nonce}' 'https://www.youtube.com/'`,
+    `frame-src 'self' 'nonce-${nonce}' 'https://www.youtube.com/embed/OYDhGj6FH5g?si=PASiV8BTfPzpPtfh'`,
     `object-src 'none'`,
     `base-uri 'self'`,
   ];

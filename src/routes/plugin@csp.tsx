74c746c347bd750b7fb1a72ed9420b07d936956a
ArthurSegato <github.reentry594@passinbox.com>
2024-01-23 21:25:57 +0000

    Improve nonce generation

--- src/routes/plugin@csp.tsx
+++ src/routes/plugin@csp.tsx
@@ -1,9 +1,10 @@
 import type { RequestHandler } from "@builder.io/qwik-city";
 import { isDev } from "@builder.io/qwik/build";
+const crypto = require("crypto");
 
 export const onRequest: RequestHandler = (event) => {
   if (isDev) return; // Will not return CSP headers in dev mode
-  const nonce = Date.now().toString(36); // Your custom nonce logic here
+  const nonce = crypto.randomBytes(16).toString("base64");
   event.sharedMap.set("@nonce", nonce);
   const csp = [
     `default-src 'self' 'unsafe-inline'`,

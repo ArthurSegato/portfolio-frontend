a7e7fe90d7f3a21c48b7cde2c900a671300be4a0
ArthurSegato <github.reentry594@passinbox.com>
2024-01-29 00:29:45 +0000

    Add security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,8 +10,9 @@ export default defineNuxtConfig({
   },
   security: {
     headers: {
-      crossOriginEmbedderPolicy:
-        process.env.NODE_ENV === "development" ? "unsafe-none" : "require-corp",
+      strictTransportSecurity: {
+        preload: true,
+      },
     },
   },
   runtimeConfig: {

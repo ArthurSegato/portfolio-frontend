23aafb3d47efd5c2089eb57f2449e995053c2204
Arthur Segato <github.reentry594@passinbox.com>
2024-05-07 16:35:13 +0100

    OK, I finished

--- components/OgImage/Projects.vue
+++ components/OgImage/Projects.vue
@@ -9,7 +9,7 @@ withDefaults(
     {
         title: "",
         description: "",
-        image: "",
+        image: "/img/02.jpg",
         date: "",
     },
 );
@@ -20,12 +20,7 @@ withDefaults(
         <div
             class="fixed inset-0 z-20 flex h-full w-full flex-row gap-10 bg-gradient-to-b from-transparent to-[#070d01] to-70% p-10">
             <div class="relative w-full rounded-xl shadow">
-                <img :src="image" class="h-full w-full rounded-xl object-cover" v-if="image" />
-                <div v-else class="texture flex w-full h-full items-center justify-center border-0 p-0">
-                    <div class="m-2 p-2 text-current bg-[#070d01]">
-                        <i>¯\_(ツ)_/¯</i>
-                    </div>
-                </div>
+                <img :src="image" class="h-full w-full rounded-xl object-cover" />
             </div>
             <div class="h-full w-full justify-end">
                 <div class="flex flex-row w-full items-end justify-between">
@@ -47,12 +42,17 @@ withDefaults(
         <div class="fixed inset-0 z-10 h-full w-full bg-black/30"></div>
         <div class="fixed h-[100%] w-[110%] -translate-x-[5%] -translate-y-[5%]">
             <img :src="image" class="h-full w-full object-cover object-center blur" v-if="image" />
-            <img src="/img/02.jpg" class="h-full w-full object-cover object-center blur" v-else />
         </div>
     </div>
 </template>
 
 
 <style>
-.texture {}
+.texture {
+    background-image: repeating-linear-gradient(-45deg,
+            transparent,
+            transparent 8px,
+            currentcolor 8px,
+            currentcolor 10px);
+}
 </style>

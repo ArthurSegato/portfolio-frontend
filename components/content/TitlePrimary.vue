478adf753923eb109e4441e345e74fa2eb6fb45a
Arthur Segato <github.reentry594@passinbox.com>
2024-05-16 00:10:31 +0100

    Pregenerate stuff

--- components/content/TitlePrimary.vue
+++ components/content/TitlePrimary.vue
@@ -1,22 +1,17 @@
 <script setup lang="ts">
 defineProps({
   title: String,
-  date: String,
+  date: Number,
 });
 </script>
 
 <template>
   <div class="my-4">
     <div class="flex w-full items-end justify-between">
-      <h1
-        class="w-full break-words break-all text-5xl font-black transition-all ease-in"
-      >
+      <h1 class="w-full break-words break-all text-5xl font-black transition-all ease-in">
         {{ title }}
       </h1>
-      <time
-        :datetime="date"
-        class="border-r-4 border-current pr-2 text-lg font-thin"
-      >
+      <time :datetime="date" class="border-r-4 border-current pr-2 text-lg font-thin">
         <i>
           {{ date }}
         </i>
--- pages/projects/[...slug].vue
+++ pages/projects/[...slug].vue
@@ -17,55 +17,32 @@ defineOgImageComponent(
 </script>
 
 <template>
-  <NuxtLink
-    hidden
-    to="/"
-    class="my-2 flex w-full select-none items-center gap-1 rounded-md border border-current px-4 py-2 transition-all ease-in hover:gap-0 lg:w-fit"
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="25"
-      height="25"
-      viewBox="0 0 24 24"
-    >
-      <path
-        fill="none"
-        stroke="currentColor"
-        stroke-dasharray="10"
-        stroke-dashoffset="10"
-        stroke-linecap="round"
-        stroke-width="2"
-        d="M8 12L15 5M8 12L15 19"
-      >
-        <animate
-          fill="freeze"
-          attributeName="stroke-dashoffset"
-          dur="0.3s"
-          values="10;0"
-        />
-      </path>
-    </svg>
-    <p>back</p>
-  </NuxtLink>
-  <article class="w-full leading-relaxed">
-    <ContentDoc>
-      <template #not-found>
-        <h1 class="text-5xl font-black">Awkward...</h1>
-        <h2 class="text-4xl font-extrabold">Hmmmmm, well...</h2>
-        <h3 class="text-3xl font-bold">that's called a error handling</h3>
-        <h4 class="text-2xl font-semibold">anyway, here goes a cat</h4>
-        <video
-          class="w-full lg:max-w-2xl"
-          autoplay
-          muted
-          loop
-          title="cat, what did you expect?"
-        >
-          <source src="/vid/neko.mp4" type="video/mp4" />
-        </video>
-        <h5 class="text-xl font-medium">ok, go back now</h5>
-        <h6 class="text-xs">please</h6>
-      </template>
-    </ContentDoc>
-  </article>
+  <div>
+    <NuxtLink hidden to="/"
+      class="my-2 flex w-full select-none items-center gap-1 rounded-md border border-current px-4 py-2 transition-all ease-in hover:gap-0 lg:w-fit">
+      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
+        <path fill="none" stroke="currentColor" stroke-dasharray="10" stroke-dashoffset="10" stroke-linecap="round"
+          stroke-width="2" d="M8 12L15 5M8 12L15 19">
+          <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="10;0" />
+        </path>
+      </svg>
+      <p>back</p>
+    </NuxtLink>
+    <article class="w-full leading-relaxed">
+      <ContentDoc>
+        <template #not-found>
+          <h1 class="text-5xl font-black">Awkward...</h1>
+          <h2 class="text-4xl font-extrabold">Hmmmmm, well...</h2>
+          <h3 class="text-3xl font-bold">that's called a error handling</h3>
+          <h4 class="text-2xl font-semibold">anyway, here goes a cat</h4>
+          <video class="w-full lg:max-w-2xl" autoplay muted loop title="cat, what did you expect?">
+            <source src="/vid/neko.mp4" type="video/mp4" />
+          </video>
+          <h5 class="text-xl font-medium">ok, go back now</h5>
+          <h6 class="text-xs">please</h6>
+        </template>
+      </ContentDoc>
+    </article>
+  </div>
+
 </template>

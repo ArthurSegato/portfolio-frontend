2b05b337f98dabab18e79f59b860cae6f2f2ae8b
Arthur Segato <arthursegato@protonmail.com>
2025-01-12 05:04:36 +0000

    Improve semantics

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,23 +1,27 @@
-<script lang="ts" setup>
-
-</script>
-
 <template>
-    <main class="bg-[#222222] text-[#D4D4D4] w-full min-h-screen flex items-center justify-center">
-        <section>
-            <article>
-                <div class="flex items-end">
+    <main class="bg-[#222222] text-[#D4D4D4] w-full min-h-screen flex p-4 box-border">
+        <section class="w-full flex items-center justify-center">
+            <article lang="en">
+                <header class="flex items-end py-5">
                     <h1 class="text-4xl font-bold">Arthur Segato</h1>
-                    <h2 class="text-2xl">.dev</h2>
-                </div>
-                <p>There's a Secret in this Website.</p>
-                <p>It's in the last place you would look.</p>
-                <p>It isn't in the text.</p>
-                <p>It's hidden in this <em>image.</em></p>
+                    <h2 class="text-2xl">.<del>game</del>dev</h2>
+                </header>
+                <blockquote>
+                    <p>"There's a Secret in this Website.</p>
+                    <p>It's in the last place you would look.</p>
+                    <p>It isn't in the text.</p>
+                    <p>It's hidden in this <em>image.</em>"</p>
+                </blockquote>
+                <footer class="text-end">
+                    <sub>(modified from <cite>What Remains of Edith Finch</cite>, 2017).</sub>
+                </footer>
             </article>
         </section>
-        <section>
-            <img src="" alt="coded image">
+        <section class="w-full flex items-center justify-center">
+            <figure>
+                <img src="" alt="coded image">
+                <figcaption class="border-l-3 border-current pl-3"><code class="bg-current/10 p-2 rounded-md font-mono">Checksum: 994347f6d374a5c1c28646ed24866bb2443fc2d191399af87c88ed1e5d16d85a</code></figcaption>
+            </figure>
         </section>
     </main>
 </template>

9bd9bfaecd28fde60efb8e849e237f077de222bc
Arthur Segato <arthursegato@protonmail.com>
2025-01-12 04:02:00 +0000

    Add initial page

---
+++ app/pages/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <main class="bg-[#222222] text-[#D4D4D4] w-full min-h-screen flex items-center justify-center">
+        <section>
+            <article>
+                <div class="flex items-end">
+                    <h1 class="text-4xl font-bold">Arthur Segato</h1>
+                    <h2 class="text-2xl">.dev</h2>
+                </div>
+                <p>There's a Secret in this Website.</p>
+                <p>It's in the last place you would look.</p>
+                <p>It isn't in the text.</p>
+                <p>It's hidden in this <em>image.</em></p>
+            </article>
+        </section>
+        <section>
+            <img src="" alt="coded image">
+        </section>
+    </main>
+</template>
